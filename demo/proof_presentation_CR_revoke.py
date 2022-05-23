525daa81ee208a80f7da88aca8a0601fb0c05f4c
Rafaadan <84093801+Rafaadan@users.noreply.github.com>
2022-05-23 14:52:36 +0200

    Update proof_presentation_CR_revoke.py

--- demo/proof_presentation_CR_revoke.py
+++ demo/proof_presentation_CR_revoke.py
@@ -1,27 +1,31 @@
 import os
 import subprocess
+import asyncio
 
-credenciales = [10, 20, 50, 100, 150, 200, 250, 300, 400, 500]
-pruebas = 25
+async def main(
+   tails_server_base_url: str = None, #url del servidor tails para las revocaciones
+):
+    credenciales = [10, 20, 50, 100, 150, 200, 250, 300, 400, 500]
+    pruebas = 25
 
-pid = os.getpid()
+    pid = os.getpid()
 
-for cred in credenciales:
-    for prueba in range(1, pruebas + 1):
+    for cred in credenciales:
+        for prueba in range(1, pruebas + 1):
 
-        if(not os.path.exists(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales")):
-            os.makedirs(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales")
-            os.makedirs(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales/datosCPUyRAM")
-        cpu_process = subprocess.Popen(["bash", "cpu_y_ram.sh", f"{pid}", f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales/datosCPUyRAM/CPU_{cred}_credenciales_prueba_{prueba}.txt"])
-        p = subprocess.Popen(["bash", "run_demo", "performance", "--count", f"{cred}", "--proof_presentation", "--revocation", "--revoke_credentials", "--publish_revocations_at_once", "--tails-server-base-url", f"{tails_server_base_url}" ], stdout=subprocess.PIPE, text= True)
-        file = open(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales/prueba{prueba}_con_{cred}_credenciales.txt","w")
-        file.write(p.communicate()[0])
-        file.close()
+            if(not os.path.exists(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales")):
+                os.makedirs(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales")
+                os.makedirs(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales/datosCPUyRAM")
+            cpu_process = subprocess.Popen(["bash", "cpu_y_ram.sh", f"{pid}", f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales/datosCPUyRAM/CPU_{cred}_credenciales_prueba_{prueba}.txt"])
+            p = subprocess.Popen(["bash", "run_demo", "performance", "--count", f"{cred}", "--proof_presentation", "--revocation", "--revoke_credentials", "--publish_revocations_at_once", "--tails-server-base-url", f"{tails_server_base_url}" ], stdout=subprocess.PIPE, text= True)
+            file = open(f"/home/rafa/aries-cloudagent-python/demo/pruebas/CR/revocando/{cred}_credenciales/prueba{prueba}_con_{cred}_credenciales.txt","w")
+            file.write(p.communicate()[0])
+            file.close()
 
-        #matar proceso cpu y ram
-        cpu_process.kill()
+            #matar proceso cpu y ram
+            cpu_process.kill()
 
-print("Ejecución finalizada")
+    print("Ejecución finalizada")
 
 if __name__ == "__main__":
     import argparse
